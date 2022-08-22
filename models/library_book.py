98a5242ec0a324bf6eac2c8753aa1c1ff8519a51
Erik <espitzner@pavlovmedia.com>
2022-08-22 15:27:11 -0500

    Chapter 04

--- models/library_book.py
+++ models/library_book.py
@@ -12,3 +12,30 @@ class LibraryBook(models.Model):
     short_name = fields.Char('Short Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection([
+        ('draft', 'Not Available'),
+        ('available', 'Available'),
+        ('lost', 'Lost')],
+        'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float('Reader Average Rating', digits=(14,4))
+    cost_price = fields.Float('Book Cost', digits='Book Price')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    retail_price = fields.Monetary('Retail Price')
+    publisher_id = fields.Many2one('res.partner', string='Publsiher')
+    author_ids = fields.Many2many('res.partner', string='Authors')
+    category_id = fields.Many2one('library.book.category')
+
+
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
+    authored_book_ids = fields.Many2many('library.book', string='Authored Books')
